bfddd1ae7613ee15840157674acc5e07794a5bfa
Anmol <anmol@Anmols-MacBook-Pro.local>
2021-10-11 12:02:22 -0400

    Graph algorithms first commit.

---
+++ algorithms/src/Cycle.java
@@ -0,0 +1,105 @@
+import org.jgrapht.Graph;
+import org.jgrapht.generate.GnmRandomGraphGenerator;
+import org.jgrapht.generate.GraphGenerator;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DefaultUndirectedGraph;
+import org.jgrapht.util.SupplierUtil;
+
+import java.util.ArrayList;
+
+public class Cycle {
+    private final int nodes;
+    private final int edges;
+    private Graph<Integer, DefaultEdge> g;
+    private ArrayList<Integer> cycle;
+
+    public Cycle(int nodes, int edges) {
+        this.nodes = nodes;
+        this.edges = edges;
+    }
+
+    public Graph<Integer, DefaultEdge>  createGraph() {
+        GraphGenerator<Integer, DefaultEdge, Integer> gen = new GnmRandomGraphGenerator<>(nodes, edges);
+        g = new DefaultUndirectedGraph<>(SupplierUtil.createIntegerSupplier(),
+                SupplierUtil.createDefaultEdgeSupplier(),
+                false);
+        gen.generateGraph(g);
+        return g;
+    }
+
+    public Boolean isCyclicUtil(int v, int parent, Boolean[] visited) {
+        // Mark the current node as visited
+        visited[v] = true;
+
+        // Recur for all the vertices adjacent to this vertex
+        for (DefaultEdge edge : g.edgesOf(v)) {
+
+            int neighbor = g.getEdgeSource(edge);
+            if(neighbor == v)
+                neighbor = g.getEdgeTarget(edge);
+            if (!visited[neighbor]) {
+                cycle.add(neighbor);
+                if (isCyclicUtil(neighbor, v, visited)) return true;
+            }
+            // If an adjacent is visited and not parent of current vertex, then there is a cycle.
+            else if (neighbor != parent) {
+                cycle.add(neighbor);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Returns true if the graph contains a cycle, else false.
+    public Boolean hasCycle() {
+        int vertices = g.vertexSet().size();
+
+        // Mark all the vertices as not visited and not part of recursion stack
+        Boolean[] visited = new Boolean[vertices + 1];
+        for (int i = 0; i < vertices; i++) {
+            visited[i] = false;
+        }
+
+        // Call the recursive helper function to detect cycle in different DFS trees
+        for (int node : g.vertexSet()) {
+            // Don't recur for u if already visited
+            cycle = new ArrayList<>();
+            if (!visited[node]) {
+                cycle.add(node);
+                if (isCyclicUtil(node, -1, visited)) return true;
+            }
+        }
+        return false;
+    }
+
+    public void printCycle() {
+        for(int v: cycle) {
+            System.out.print(v + "~");
+        }
+    }
+
+}
+
+class CycleClient {
+    public static void main(String[] args) {
+        int[] testVertices = new int[]{10, 50, 100, 125, 500, 760, 1000, 2500, 5000, 6000, 10000, 13000, 23000, 50000};
+        int[] testEdges = new int[]{20, 70, 150, 170, 700, 850, 1400, 3000, 5800, 7000, 15000, 15000, 27000, 58000};
+        Cycle[] testCase = new Cycle[testVertices.length];
+        for(int i=0; i< testVertices.length; i++){
+            testCase[i] = new Cycle(testVertices[i], testEdges[i]);
+            testCase[i].createGraph();
+            long start = System.nanoTime();
+            boolean result = testCase[i].hasCycle();
+            long finish = System.nanoTime();
+            long timeElapsed = finish - start;
+            if(result)
+                System.out.println("\n" + timeElapsed + " nanoseconds elapsed in finding a cycle for " +
+                        testVertices[i] + " vertices" + " and " + testEdges[i] + " edges");
+            //Print the knot
+            testCase[i].printCycle();
+        }
+
+    }
+
+
+}
---
+++ algorithms/src/MST.java
@@ -0,0 +1,129 @@
+import org.jgrapht.Graph;
+import org.jgrapht.alg.spanning.KruskalMinimumSpanningTree;
+import org.jgrapht.generate.GnmRandomGraphGenerator;
+import org.jgrapht.generate.GraphGenerator;
+import org.jgrapht.graph.DefaultUndirectedWeightedGraph;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.util.SupplierUtil;
+import java.util.Random;
+
+import java.util.*;
+
+import static java.lang.System.nanoTime;
+
+
+class MST {
+    private final int nodes;
+    private final int edges;
+    private static Graph<Integer, DefaultWeightedEdge> g;
+    private DefaultWeightedEdge maxEdge;
+
+    public MST(int nodes, int edges) {
+        this.nodes = nodes;
+        this.edges = edges;
+    }
+
+    public Graph<Integer, DefaultWeightedEdge>  createGraph() {
+        GraphGenerator<Integer, DefaultWeightedEdge, Integer> gen = new GnmRandomGraphGenerator<>(nodes, edges);
+        g = new DefaultUndirectedWeightedGraph<>(SupplierUtil.createIntegerSupplier(),
+                SupplierUtil.createDefaultWeightedEdgeSupplier());
+        gen.generateGraph(g);
+        return g;
+    }
+
+    public void setWeightsForGraph() {
+        for (DefaultWeightedEdge e : g.edgeSet()) {
+            g.setEdgeWeight(e, getRandomWeightBetween(3,12));
+        }
+    }
+
+    public int getRandomWeightBetween(int min, int max) {
+        Random rand = new Random();
+        int upperBound = max - min + 1;
+        return min + rand.nextInt(upperBound);
+    }
+
+    public Set<DefaultWeightedEdge> findMinimumSpanningTreeLibrary(Graph<Integer, DefaultWeightedEdge> graph) {
+        Set<DefaultWeightedEdge> min_span_edges = new HashSet<>();
+        for (DefaultWeightedEdge e : new KruskalMinimumSpanningTree<>(graph).getSpanningTree()) {
+            min_span_edges.add(e);
+        }
+        return min_span_edges;
+    }
+
+    public Boolean isCyclicUtil(int v, int parent, Boolean[] visited, DefaultWeightedEdge maxEdge) {
+        // Mark the current node as visited
+        visited[v] = true;
+
+        // Recur for all the vertices adjacent to this vertex
+        for (DefaultWeightedEdge edge : g.edgesOf(v)) {
+
+            int neighbor = g.getEdgeSource(edge);
+            if(neighbor == v)
+                neighbor = g.getEdgeTarget(edge);
+            if( g.getEdgeWeight(maxEdge) <= g.getEdgeWeight(edge)) maxEdge = edge;
+            if (!visited[neighbor]) {
+                if (isCyclicUtil(neighbor, v, visited, maxEdge)) return true;
+            }
+            // If an adjacent is visited and not parent of current vertex, then there is a cycle.
+            else if (neighbor != parent) {
+                g.removeEdge(maxEdge);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Returns true if the graph contains a cycle, else false.
+    public Boolean hasCycle() {
+        for(DefaultWeightedEdge e: g.edgeSet()){
+            maxEdge = e;
+            break;
+        }
+
+        // Mark all the vertices as not visited and not part of recursion stack
+        Boolean[] visited = new Boolean[nodes];
+        for (int i = 0; i < nodes; i++) {
+            visited[i] = false;
+        }
+
+        // Call the recursive helper function to detect cycle in different DFS trees
+        for (int node : g.vertexSet()) {
+            // Don't recur for u if already visited
+            if (!visited[node]) {
+                if(isCyclicUtil(node, -1, visited, maxEdge)) return true;
+            }
+        }
+        return false;
+    }
+
+    public Set<DefaultWeightedEdge> getMinimumSpanningTree() {
+
+        return new HashSet<>(g.edgeSet());
+    }
+}
+
+class MSTClient {
+    public static void main(String[] args) {
+        int[] testVertices = new int[]{10, 50, 100, 125, 500, 760, 1000, 2500, 5000, 6000, 10000, 13000, 23000, 50000};
+        MST[] testCase = new MST[testVertices.length];
+        for(int i=0; i< testVertices.length; i++){
+            testCase[i] = new MST(testVertices[i], testVertices[i]+8);
+        }
+        for(int j=0;j< testCase.length;j++) {
+            Graph<Integer, DefaultWeightedEdge> graph = testCase[j].createGraph();
+            testCase[j].setWeightsForGraph();
+            long start = nanoTime();
+            while (testCase[j].hasCycle()) {
+            }
+            long finish = nanoTime();
+            long timeElapsed = finish - start;
+            System.out.println(timeElapsed + " nanoseconds elapsed in computing MST for " +
+                    testVertices[j] + " vertices" + " and " + (testVertices[j] + 8) + " edges");
+            //Test case validation
+            if (testCase[j].findMinimumSpanningTreeLibrary(graph).size() != testCase[j].getMinimumSpanningTree().size())
+                throw new AssertionError();
+        }
+
+    }
+}
